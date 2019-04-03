76c100127b394000c999beb9249ff44306b6bea3
Kaushal Mhalgi <kaushal1998@users.noreply.github.com>
2019-04-03 11:28:55 +0530

    initial commit

--- required_files/Tokens.sol
+++ required_files/Tokens.sol
@@ -45,13 +45,8 @@ contract Tokens is EIP20Interface {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        // uint256 allowance = allowed[_from][msg.sender];
-        // require(balances[_from] >= _value && allowance >= _value);
         balances[_to] += _value;
         balances[_from] -= _value;
-        // if (allowance < MAX_UINT256) {
-        //     allowed[_from][msg.sender] -= _value;
-        // }
         emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
         return true;
     }
